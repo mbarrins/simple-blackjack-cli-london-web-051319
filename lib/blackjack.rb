c1d6d2fe4aaadfa851af9f9f9a4c3e0ccda3013d
Maija Barrins <maija.barrins@gmail.com>
2019-04-25 19:02:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,6 +52,6 @@ def runner
   card_total = initial_round
   until card_total > 21
     card_total = hit?(card_total)
-    card_total > 21 ? end_game(card_total) : display_card_total(card_total)
+    display_card_total(card_total) if card_total <= 21 
   end
 end
